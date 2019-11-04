02163d265403fa400dabea738e8589f423aa0526
Simon Pirko <sipi7gm@gmail.com>
2019-11-05 02:19:30 +0300

    change10

--- src/main/java/com/example/demo/ProcessorImpl.java
+++ src/main/java/com/example/demo/ProcessorImpl.java
@@ -21,6 +21,9 @@ public class ProcessorImpl implements Processor {
     @Override
     public SendMessage getMessage(Update update) {
         String text = update.getMessage().getText();
+        if (text.equals("/start")){
+            return sendMsg(update, "Привет, как тебя зовут?");
+        }
         if (text.equalsIgnoreCase("Привет")) {
             return sendMsg(update, "Привет " + update.getMessage().getFrom().getFirstName());
         } else if (text.equalsIgnoreCase("Как дела?")) {
