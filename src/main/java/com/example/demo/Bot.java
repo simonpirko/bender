40e84c388c459e74a39be4fe42137ff8d9fb8a97
Simon Pirko <sipi7gm@gmail.com>
2019-11-04 23:41:18 +0300

    change2

--- src/main/java/com/example/demo/Bot.java
+++ src/main/java/com/example/demo/Bot.java
@@ -3,26 +3,43 @@ package com.example.demo;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
-public class Bender extends TelegramLongPollingBot {
+public class Bot extends TelegramLongPollingBot {
 
     private final PropertyProvider propertyProvider;
 
-    public Bender(PropertyProvider propertyProvider) {
+    public Bot(PropertyProvider propertyProvider) {
         this.propertyProvider = propertyProvider;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            sendMsg(update, "Лена: Привет " + update.getMessage().getFrom().getFirstName());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            sendMsg(update, "Лена: Как дела, любимый?");
+        }
+    }
+
+    private void sendMsg(Update update, String text) {
         Message message = update.getMessage();
         SendMessage sendMessage = new SendMessage();
-        Chat chat = message.getChat();
+        sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText(chat.getUserName());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -33,5 +50,9 @@ public class Bender extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return propertyProvider.getToken();
+    }
+
+    private static class God {
+
     }
 }
